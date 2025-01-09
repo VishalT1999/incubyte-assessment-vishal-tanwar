1e94b18984c82aa8f1c824ade387ea0c3af6ce80
vishal <vishal24tanwar@gmail.com>
2025-01-09 11:13:24 +0530

    comma sapted string

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,5 +2,21 @@
 class Calculator
   def add(string)
     return 0 if string.empty?
+
+    num_list = string.split(',')
+
+    total = 0
+    negatives = []
+    num_list.each do |num_str|
+      next if num_str.empty?  
+      num = num_str.to_i
+      if num < 0
+        negatives << num
+      else
+        total += num
+      end
+    end
+
+    total
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,15 @@
 # spec/calculator_spec.rb
 require 'calculator'
 
-RSpec.describe Calculator, "#add" do
-  it "test with empty string" do
+RSpec.describe Calculator, "#test" do
+  it "with empty string" do
     calculator = Calculator.new
     expect(calculator.add('')).to eq(0)
   end
+
+  it "with normal comma sapated string" do
+    calculator = Calculator.new
+    expect(calculator.add('1,3')).to eq(4)
+  end
+  
 end
